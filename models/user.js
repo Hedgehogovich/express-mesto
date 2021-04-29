aee42e59f4fdc1527e1cfc5e30fea56ae2f8866b
Yuriy Chachilo <yureg32rus@gmail.com>
2021-04-29 08:47:32 +0300

    feat: добавление полей email и password

--- models/user.js
+++ models/user.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: (props) => `${props.value} не является корректным Email`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
   name: {
     type: String,
     required: true,
